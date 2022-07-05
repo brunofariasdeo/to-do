fa0727296fbef3d209fff7f2bf149e20656aa9c8
brunofariasdeo <jfarias@daitan.com>
2022-07-05 11:03:53 -0300

    Set Tailwind colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,23 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
       colors: {
         gray: {
+          100: "#F2F2F2",
+          200: "#D9D9D9",
+          300: "#808080",
+          400: "#333333",
+          500: "#262626",
+          600: "#1A1A1A",
           700: "#0D0D0D",
         },
+        purple: "#8284FA",
+        "purple-dark": "#5E60CE",
+        blue: "#4EA8DE",
+        "blue-dark": "#1E6F9F",
       },
     },
   },
