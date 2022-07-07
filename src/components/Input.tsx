6cdcb01f6b9a4470ac38ef1e97b65ead3550d065
brunofariasdeo <jfarias@daitan.com>
2022-07-07 12:26:24 -0300

    Create Button and Input components

---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+export function Input() {
+  return (
+    <input
+      className="w-full h-14 mr-4 pl-4 text-gray-100 text-base placeholder-gray-300 bg-gray-500 border rounded-lg border-gray-700"
+      placeholder="Adicione uma nova tarefa"
+    />
+  );
+}
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,15 +1,11 @@
-import { PlusCircle } from "phosphor-react";
+import { CreateButton } from "./CreateButton";
+import { Input } from "./Input";
 
 export function NewTask() {
   return (
     <section className="w-full flex items-center justify-center">
-      <input
-        className="w-full h-14 mr-4 pl-4 text-gray-100 text-base placeholder-gray-300 bg-gray-500 border rounded-lg border-gray-700"
-        placeholder="Adicione uma nova tarefa"
-      />
-      <button className="w-28 h-14 p-2 bg-blue-dark text-gray-100 text-sm flex items-center justify-center rounded-lg font-bold">
-        Criar <PlusCircle className="ml-1" size={20} />
-      </button>
+      <Input />
+      <CreateButton />
     </section>
   );
 }
